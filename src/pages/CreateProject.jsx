357a08508ba8dc7e28c93d2cd783d1b478821852
Kavin <kavink98@gmail.com>
2023-08-18 02:18:28 -0400

    Removed unused imports

--- src/pages/CreateProject.jsx
+++ src/pages/CreateProject.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { ethers } from "ethers";
 
 import { money } from "../assets";
 import { CustomButton, FormField, Loader } from "../components";
